bc1cf06101b8663bae204ccbe7e0677dfb69298e
Veeral Patel <vpatel98@gmail.com>
2019-05-30 21:52:04 -0700

    fixed typo in enum for status

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,5 @@
 class Ticket < ApplicationRecord
-  enum status: { low: 0, medium: 1, high: 2}
+  enum status: { open: 0, in_progress: 1, closed: 2}
   belongs_to :incident, required: true
 
   validates :name, presence: true
