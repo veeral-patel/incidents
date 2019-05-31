9ea662393071c405296028d4a509ed03451d18cf
Veeral Patel <vpatel98@gmail.com>
2019-05-30 22:26:18 -0700

    added leads method to Incident model

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -2,6 +2,10 @@ class Incident < ApplicationRecord
     validates :name, presence: true
     has_many :tickets
 
+    def leads
+        self.tickets.where(is_lead: false)
+    end
+
     def to_s
         self.name
     end
