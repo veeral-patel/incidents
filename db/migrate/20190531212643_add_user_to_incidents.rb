074e9d37c0993a192e131ec988dd8cd9e4fb5c5a
Veeral Patel <vpatel98@gmail.com>
2019-05-31 14:28:27 -0700

    replace a commit which had the wrong name

--- db/migrate/20190531212643_add_user_to_incidents.rb
+++ db/migrate/20190531212643_add_user_to_incidents.rb
@@ -1,4 +1,4 @@
-class AddUserToCases < ActiveRecord::Migration[5.2]
+class AddUserToIncidents < ActiveRecord::Migration[5.2]
   def change
     add_reference :incidents, :user, foreign_key: true
   end
