9d59cdcc89c9917cb1c391f58e957afffa41d6c6
Veeral Patel <vpatel98@gmail.com>
2019-06-01 16:05:15 -0700

    created tabs for show ticket properly

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,28 +1,28 @@
 class AttachmentsController < ApplicationController
   before_action :set_attachment, only: [:show, :edit, :update, :destroy]
+  before_action :set_ticket, only: [:index]
 
-  # GET /attachments
-  # GET /attachments.json
+  # GET /tickets/1/attachments
+  # GET /tickets/1/attachments.json
   def index
-    @attachments = Attachment.all
   end
 
-  # GET /attachments/1
-  # GET /attachments/1.json
+  # GET /tickets/1/attachments/1
+  # GET /tickets/1/attachments/1.json
   def show
   end
 
-  # GET /attachments/new
+  # GET /tickets/1/attachments/new
   def new
     @attachment = Attachment.new
   end
 
-  # GET /attachments/1/edit
+  # GET /tickets/1/attachments/1/edit
   def edit
   end
 
-  # POST /attachments
-  # POST /attachments.json
+  # POST /tickets/1/attachments
+  # POST /tickets/1/attachments.json
   def create
     @attachment = Attachment.new(attachment_params)
 
@@ -37,8 +37,8 @@ class AttachmentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /attachments/1
-  # PATCH/PUT /attachments/1.json
+  # PATCH/PUT /tickets/1/attachments/1
+  # PATCH/PUT /tickets/1/attachments/1.json
   def update
     respond_to do |format|
       if @attachment.update(attachment_params)
@@ -51,8 +51,8 @@ class AttachmentsController < ApplicationController
     end
   end
 
-  # DELETE /attachments/1
-  # DELETE /attachments/1.json
+  # DELETE /tickets/1/attachments/1
+  # DELETE /tickets/1/attachments/1.json
   def destroy
     @attachment.destroy
     respond_to do |format|
@@ -64,7 +64,11 @@ class AttachmentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_attachment
-      @attachment = Attachment.find(params[:id])
+      @attachment =Attachment.find(params[:id])
+    end
+
+    def set_ticket
+      @ticket = Ticket.find(params[:ticket_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
