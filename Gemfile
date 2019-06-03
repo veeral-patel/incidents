2c7cee6f9afdfad0fe37fa5f220219e660b4421f
Veeral Patel <vpatel98@gmail.com>
2019-06-03 11:23:18 -0700

    turned incidents table into a datatable

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'devise'
 gem 'ancestry'
 gem 'acts-as-taggable-on', '~> 6.0'
+gem 'jquery-rails'
+gem 'jquery-datatables'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require datatables
 //= require_tree .
