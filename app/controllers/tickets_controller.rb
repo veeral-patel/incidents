874c8dd603be38d6c823d1a6e7c426b6436a7635
Veeral Patel <vpatel98@gmail.com>
2019-06-07 13:46:20 -0700

    added incident menu to incident details

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -85,6 +85,7 @@ class TicketsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_ticket
       @ticket = Ticket.find(params[:id])
+      @incident = @ticket.incident
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
