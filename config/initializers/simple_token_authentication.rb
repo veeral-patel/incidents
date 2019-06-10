6a6404c58d96c66b032df09b399c6103116ce739
Veeral Patel <vpatel98@gmail.com>
2019-06-10 00:34:23 -0700

    change X-User-Email header to X-User-Username

--- config/initializers/simple_token_authentication.rb
+++ config/initializers/simple_token_authentication.rb
@@ -1,3 +1,3 @@
 SimpleTokenAuthentication.configure do |config|
-    config.header_names = { user: { authentication_token: 'X-User-Token', email: 'X-User-Email' } }
+    config.identifiers = { user: 'username' }
 end
