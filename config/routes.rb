ad2a41db7ec537ac4be31fe137eee862f570ca41
Veeral Patel <vpatel98@gmail.com>
2019-06-13 11:10:40 -0700

    edit profile works once again

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   devise_for :users, :skip => [:registrations] 
   as :user do
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    put 'users/:id' => 'devise/registrations#update', :as => 'user_registration'
   end
 
 
