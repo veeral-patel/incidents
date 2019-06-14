333961a9585d695d3a9ff9364d84ff0c4051b2b8
Veeral Patel <vpatel98@gmail.com>
2019-06-14 11:59:20 -0700

    test valid and invalid incident

--- test/models/incident_test.rb
+++ test/models/incident_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class IncidentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid incident' do
+    creator = users(:user_1)
+
+    # status can also be a number (0-2) or "open"/"closed"/"in_progress"
+    incident = Incident.new(name: "An incident", status: :in_progress, user: creator)
+
+    assert incident.valid?
+  end
+
+  test 'invalid incident' do
+    incident = Incident.new()
+
+    refute incident.valid?
+
+    assert_equal incident.errors.size, 3
+    assert_not_nil incident.errors[:name], 'no validation error for name present'
+    assert_not_nil incident.errors[:status], 'no validation error for status present'
+    assert_not_nil incident.errors[:user], 'no validation error for user present'
+  end
 end
