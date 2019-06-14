d5db54de3aed2137ea8cd0646cc08e9c9ab15bed
Veeral Patel <vpatel98@gmail.com>
2019-06-14 12:31:53 -0700

    added ticket tests

--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class TicketTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid ticket' do
+    ticket = Ticket.new(
+       name: "Ticket one",
+       status: :open,
+       priority: :high,
+       user: users(:user_1),
+       incident: incidents(:incident_1)
+    )
+    assert ticket.valid?
+  end
+
+  test 'can optionally assign a ticket' do
+    ticket = Ticket.new(
+       name: "Ticket one",
+       status: :open,
+       priority: :high,
+       user: users(:user_1),
+       assigned_to: users(:user_1),
+       incident: incidents(:incident_1),
+    )
+    assert ticket.valid?
+  end
+
+  test 'invalid ticket' do
+    ticket = Ticket.new()
+
+    refute ticket.valid?
+
+    assert_equal ticket.errors.size, 5
+    assert_not_nil ticket.errors[:name], 'no validation error for name present'
+    assert_not_nil ticket.errors[:status], 'no validation error for status present'
+    assert_not_nil ticket.errors[:priority], 'no validation error for priority present'
+    assert_not_nil ticket.errors[:user], 'no validation error for user present'
+    assert_not_nil ticket.errors[:incident], 'no validation error for incident present'
+  end
+
+  test 'to string' do
+    ticket = tickets(:ticket_1)
+    assert ticket.to_s, "Ticket One"
+  end
 end
