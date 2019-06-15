15102884175c3094aa09943b5053c34fa847199b
Veeral Patel <vpatel98@gmail.com>
2019-06-14 17:48:37 -0700

    copy/pasted incident > tickets into incident > members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,12 @@
 class MembersController < ApplicationController
+    before_action :set_incident
+
     def index
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_incident
+      @incident = Incident.find(params[:incident_id])
     end
 end
