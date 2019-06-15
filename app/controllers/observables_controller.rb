59be2c1117aa45f2b12959356544fc6eec507244
Veeral Patel <vpatel98@gmail.com>
2019-06-15 14:06:38 -0700

    add and enforce observables policy

--- app/controllers/observables_controller.rb
+++ app/controllers/observables_controller.rb
@@ -1,4 +1,8 @@
 class ObservablesController < ApplicationController
+  # authorize each action
+  before_action :authorize_actions_on_one_observable, except: [:index, :create, :new]
+  before_action :authorize_other_actions, only: [:index, :create, :new]
+
   before_action :set_observable, only: [:show, :update, :destroy]
   before_action :set_ticket
   before_action :set_current_user
@@ -71,6 +75,15 @@ class ObservablesController < ApplicationController
 
     def set_current_user
         @current_user = current_user
+    end
+
+    def authorize_actions_on_one_observable
+      @observable = Observable.find(params[:id])
+      authorize @observable
+    end
+
+    def authorize_other_actions
+      authorize Observable
     end
 
     def observable_params
