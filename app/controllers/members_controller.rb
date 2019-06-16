6b5efd9aac8ec1bc42efd22d4ae43ca0056e3b06
Veeral Patel <vpatel98@gmail.com>
2019-06-15 18:47:47 -0700

    add authorization to members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,10 +1,14 @@
+# remember: after adding an action, add authorization checking code for it
 class MembersController < ApplicationController
     before_action :set_incident
 
     def index
+      raise Pundit::NotAuthorizedError unless IncidentPolicy.new(current_user, @incident).show?
     end
 
     def destroy
+      raise Pundit::NotAuthorizedError unless IncidentPolicy.new(current_user, @incident).show?
+
       if @incident.members.count == 1
         respond_to do |format|
           format.html { redirect_to incident_members_url, alert: "Cannot remove last member" }
