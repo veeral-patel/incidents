74a0a5eb8bc3f80619094f8cd7ce350524f1f1e8
Veeral Patel <vpatel98@gmail.com>
2019-06-15 18:23:28 -0700

    add useless policies

--- app/policies/ticket_policy.rb
+++ app/policies/ticket_policy.rb
@@ -1,18 +1,4 @@
 class TicketPolicy < ApplicationPolicy
-    # not specific to one ticket ------
-    def index?
-        true
-    end
-
-    def new?
-        true
-    end
-
-    def create?
-        true
-    end
-
-    # specific to one ticket -------
     def show?
         # can show a ticket iff you can show its incident
         IncidentPolicy.new(@user, record.incident).show?
