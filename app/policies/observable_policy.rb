394f0686179c4c9a3415b0eb33ac831d59a01223
Veeral Patel <vpatel98@gmail.com>
2019-06-15 17:17:01 -0700

    authorize index, new, create in ticket > observables

--- app/policies/observable_policy.rb
+++ app/policies/observable_policy.rb
@@ -1,18 +1,4 @@
 class ObservablePolicy < ApplicationPolicy
-    # not specific to one observable ---
-    def index?
-        true
-    end
-
-    def new?
-        true
-    end
-
-    def create?
-        true
-    end
-
-    # specific to one observable -----
     def show?
         # can show a comment if you can show its ticket
         TicketPolicy.new(@user, record.ticket).show?
