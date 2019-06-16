d4724eb96120e5a21822463c5908b0c266ca1e8d
Veeral Patel <vpatel98@gmail.com>
2019-06-15 17:41:55 -0700

    removed useless policies

--- app/policies/attachment_policy.rb
+++ app/policies/attachment_policy.rb
@@ -1,18 +1,4 @@
 class AttachmentPolicy < ApplicationPolicy
-    # not specific to one attachment ---
-    def index?
-        true
-    end
-
-    def new?
-        true
-    end
-
-    def create?
-        true
-    end
-
-    # specific to one attachment ------
     def show?
         # can show an attachment if you can show its ticket
         TicketPolicy.new(@user, record.ticket).show?
