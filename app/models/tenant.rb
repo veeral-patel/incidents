76445e4b8dc59cd4dc9440b80591b46465e56448
Veeral Patel <vpatel98@gmail.com>
2019-07-22 19:29:54 -0700

    add callbacks to create/delete tenants

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,3 +1,15 @@
 class Tenant < ApplicationRecord
-    validates :name, presence: true, uniqueness: true, with: /^[A-Za-z0-9]+$/
+    validates :name, presence: true, uniqueness: true, format: { with: /\A[A-Za-z0-9]+\z/ }
+
+    after_create :create_tenant
+    after_destroy :destroy_tenant
+
+    private
+        def create_tenant
+            Apartment::Tenant.create(name)
+        end
+
+        def destroy_tenant
+            Apartment::Tenant.drop(name)
+        end
 end
