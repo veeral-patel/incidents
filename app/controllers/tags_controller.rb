11c2ccacbbd22e1b88d919cdaa95fe7f314f4cd3
Veeral Patel <vpatel98@gmail.com>
2019-07-29 15:57:56 -0700

    add additional options

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,8 @@
 class TagsController < ApplicationController
     def index
-        # always responds with JSON, even if request type is HTML
         @all_tags = ActsAsTaggableOn::Tag.all
-        render json: @all_tags
+        respond_to do |format|
+            format.json { render json: @all_tags }
+        end
     end
 end
