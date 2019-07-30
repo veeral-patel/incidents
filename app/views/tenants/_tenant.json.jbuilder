bfb56a5050ddf4c91e6376dae07e81700d578492
Veeral Patel <vpatel98@gmail.com>
2019-07-30 14:46:36 -0700

    remove apartment-related files

--- app/views/tenants/_tenant.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! tenant, :id, :name, :created_at, :updated_at
-json.url tenant_url(tenant, format: :json)
--- db/migrate/20190723012841_create_tenants.rb
+++
@@ -1,9 +0,0 @@
-class CreateTenants < ActiveRecord::Migration[5.2]
-  def change
-    create_table :tenants do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
