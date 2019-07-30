5431de63841a2f51269cd543daa118f3effa76ba
Veeral Patel <vpatel98@gmail.com>
2019-07-30 00:58:33 -0700

    print out contents of .env

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 # initialize environment variables
 secret=$(dd if=/dev/urandom count=1 bs=128 2> /dev/null | sha512sum | cut -d ' ' -f 1)
 echo 'SECRET_KEY_BASE='$secret > .env
+echo 'Initialized .env with contents:'
+echo ""
+cat .env
+echo ""
 
 # to use the dark theme, replace with:
 # sh ./choose_theme.sh dark
