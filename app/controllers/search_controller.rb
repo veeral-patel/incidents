5b64429206a091b8c5e4b8347a50575504087849
Veeral Patel <vpatel98@gmail.com>
2019-07-31 10:02:24 -0700

    indicate the search query and number of results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
     def index
         if params["q"]
-            @results = PgSearch.multisearch(params["q"]).with_pg_search_highlight
+            @query = params["q"]
+            @results = PgSearch.multisearch(@query).with_pg_search_highlight
             @results = @results.paginate(:page => params[:page], :per_page => 10)
         else
             @results = []
