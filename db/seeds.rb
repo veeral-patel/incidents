0000f297e9828609e960bb2ee736c53b42e96125
Veeral Patel <vpatel98@gmail.com>
2019-07-30 17:17:34 -0700

    create tickets, too

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,29 @@
-1.times do
-    user = User.create(
+# create 5 users
+5.times do
+    User.create(
        username: Faker::Internet.unique.username,
        email: Faker::Internet.unique.email,
        password: Faker::Internet.password
     )
+end
 
-    incident = user.incidents.create(
+# create 5 incidents
+5.times do
+    random_user = User.all.sample
+    random_user.incidents.create(
         name: "Incident-#{Faker::Code.nric}",
         status: [:open, :closed, :in_progress].sample,
-        user: user
+    )
+end
+
+# create 25 tickets
+25.times do
+    random_user = User.all.sample
+    random_user.tickets.create(
+        name: "Ticket-#{Faker::Code.nric}",
+        incident: Incident.all.sample,
+        status: [:open, :closed, :in_progress].sample,
+        priority: [:high, :medium, :low].sample,
     )
 end
 
