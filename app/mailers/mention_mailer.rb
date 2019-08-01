ebc6023e344db9113024b58642c17edf87e821cd
Veeral Patel <vpatel98@gmail.com>
2019-08-01 14:15:33 -0700

    small improvements to mailer template and subject

--- app/mailers/mention_mailer.rb
+++ app/mailers/mention_mailer.rb
@@ -1,6 +1,6 @@
 class MentionMailer < ApplicationMailer
     def mentioned_in_incident_description(user, incident)
         @user, @incident = user, incident
-        mail(to: @user.email, subject: 'You were mentioned in an incident')
+        mail(to: @user.email, subject: "You were mentioned in incident '#{@incident}'")
     end
 end
