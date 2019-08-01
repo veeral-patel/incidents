42da27dd2d58aee33348d74484f97a895b0817aa
Veeral Patel <vpatel98@gmail.com>
2019-07-31 21:32:17 -0700

    add host for ActionMailer

--- config/environments/heroku.rb
+++ config/environments/heroku.rb
@@ -93,6 +93,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'tryincidents.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
