02f65f4fbad8ebd772729ce3d3d96ec848cb6118
Veeral Patel <vpatel98@gmail.com>
2019-07-31 21:47:01 -0700

    change domain for heroku environment

--- config/environments/heroku.rb
+++ config/environments/heroku.rb
@@ -93,13 +93,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'tryincidents.com' }
+  config.action_mailer.default_url_options = { host: 'demo.tryincidents.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'tryincidents.com',
+    :domain => 'demo.tryincidents.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
