bcd5638897fa9c2410b37d2f93230e1c89d47272
Veeral Patel <vpatel98@gmail.com>
2019-08-02 09:48:31 -0700

    added edit priority page

--- app/controllers/priorities_controller.rb
+++ app/controllers/priorities_controller.rb
@@ -1,13 +1,23 @@
 class PrioritiesController < ApplicationController
+    before_action :set_priority, only: [:edit]
     before_action :authorize_admin
 
+    # GET /priorities
     def index
         @priorities = Priority.all
+    end
+
+    # GET /priorities/1/edit
+    def edit
     end
 
     private
       def authorize_admin
         return unless !current_user.admin?
         redirect_to root_path, alert: 'Admins only!'
+      end
+
+      def set_priority
+        @priority = Priority.find(params[:id])
       end
 end
