63f4aa1f99ada7304c90f1dbc9812ca0d6818109
Veeral Patel <vpatel98@gmail.com>
2019-08-01 22:26:03 -0700

    add status method to ticket model

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,7 @@
 class Status < ApplicationRecord
     validates :name, presence: true, uniqueness: true
+
+    def to_s
+        self.name
+    end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -16,7 +16,6 @@ class Ticket < ApplicationRecord
   has_ancestry
   acts_as_taggable
 
-  enum status: { open: 0, in_progress: 1, closed: 2}
   enum priority: { low: 0, medium: 1, high: 2}
 
   has_many :attachments, dependent: :destroy
@@ -28,11 +27,15 @@ class Ticket < ApplicationRecord
   belongs_to :assigned_to, :class_name => 'User', optional: true
 
   validates :name, presence: true
-  validates :status, presence: true
+  validates :status_id, presence: true
   validates :priority, presence: true
 
   def to_s
     self.name
+  end
+
+  def status
+    Status.find(status_id)
   end
 
   def to_json
