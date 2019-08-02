3060e479a37fad78b760cedf407c2cd33d042b8a
Veeral Patel <vpatel98@gmail.com>
2019-08-02 09:40:08 -0700

    add priorities to DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 Status.create(name: "Open")
 Status.create(name: "In Progress")
 Status.create(name: "Closed")
+
+Priority.create(name: "Low")
+Priority.create(name: "Medium")
+Priority.create(name: "High")
