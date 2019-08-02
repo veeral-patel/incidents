9afb4b47f4af235e4e93f917c5a49091b5df4ad1
Veeral Patel <vpatel98@gmail.com>
2019-08-02 14:01:25 -0700

    email users when they're removed from an incident

--- app/mailers/incident_mailer.rb
+++ app/mailers/incident_mailer.rb
@@ -3,4 +3,9 @@ class IncidentMailer < ApplicationMailer
         @user, @incident = user, incident
         mail(to: @user.email, subject: "You were added to incident '#{@incident}'")
     end
+    
+    def removed_from_incident(user, incident)
+        @user, @incident = user, incident
+        mail(to: @user.email, subject: "You were removed from incident '#{@incident}'")
+    end
 end
