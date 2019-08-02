d25979f9ad14a370774eb42995bcd873a26924c5
Veeral Patel <vpatel98@gmail.com>
2019-08-02 09:31:36 -0700

    create template for priorities#index

--- app/controllers/priorities_controller.rb
+++ app/controllers/priorities_controller.rb
@@ -1,2 +1,13 @@
 class PrioritiesController < ApplicationController
+    before_action :authorize_admin
+
+    def index
+        @priorities = Priority.all
+    end
+
+    private
+      def authorize_admin
+        return unless !current_user.admin?
+        redirect_to root_path, alert: 'Admins only!'
+      end
 end
